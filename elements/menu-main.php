25ffd767b67678664ccffa3c26b64651b0c96064
fak34 <FreddyComputer@Freddys-MacBook-Pro-2.local>
2016-11-15 17:24:59 -0500

    Main Menu
    Styling and Html for main menu

---
+++ elements/menu-main.php
@@ -0,0 +1,28 @@
+<!-- sidebar -->
+<div class="column col-sm-3" id="sidebar">
+  <a href="index.php">
+    <img src="img/logo-white.png" style="height:100px; width:100px; margin-top:15px; margin-left: 15px; float:left;"></img>
+    <img src="img/logo.png" style="height:85px; width:150px; margin-top:25px; margin-left: 15px;"></img>
+  </a>
+  <br><hr>
+  <ul class="nav nav-main">
+    <li class="active"><a href="index.php"><h5>Home</h5></a>
+    </li>
+    <li><a href="pages/people.php"><h5>People</h5></a>
+    </li>
+    <li><a href="pages/courses.php"><h5>Courses</h5></a>
+    </li>
+    <li><a href="pages/highlights.php"><h5>News & Awards</h5></a>
+    </li>
+    <li><a href="pages/products.php"><h5>Software</h5></a>
+    </li>
+    <li><a href="pages/join.php"><h5>Join Us</h5></a>
+    </li>
+  </ul>
+  <ul class="nav hidden-xs text-center" id="sidebar-footer">
+    <li>
+      <h3>Machine Learning at Cornell</h3>
+    </li>
+    <br><br>
+  </ul>
+</div>
