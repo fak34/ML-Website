48fdf56a9b0afd2223853f3946984625a9d5002b
fak34 <FreddyComputer@Freddys-MacBook-Pro-2.local>
2016-11-15 17:24:41 -0500

    Contact
    Keeps the Contact info

---
+++ elements/contact.php
@@ -0,0 +1,17 @@
+<div class="col-sm-12">
+  <div class="page-header text-muted divider" align="center">
+    Connect with Us
+  </div>
+</div>
+
+<div class="row" style="margin-bottom:20px;">
+  <div class="col-sm-6">
+    Are you interested in Machine Learning at Cornell?
+    <p>Email us at: <b>contact@machinelearning.cornell.edu</b></p>
+  </div>
+           
+         
+  <div class="col-sm-6" align="right">
+    <p>©Machine Learning at Cornell</p>
+  </div>
+</div> 
